c4dbb213dfef2792fe06dfa3ac25606a59eccaf9
NAGARRO\mdmobin <md.mobin@nagarro.com>
2025-02-15 17:38:34 +0530

    Added Security Util

--- amcart-security/src/main/java/com/nagp/amcart/security/constant/SecurityConstant.java
+++ amcart-security/src/main/java/com/nagp/amcart/security/constant/SecurityConstant.java
@@ -5,8 +5,9 @@ public class SecurityConstant {
 
     }
     public static final String INVALID_TOKEN = "INVALID_TOKEN";
-    public static final String INVALID_TOKEN_ERROR_MESSAGE = "Invalid Token.Please refresh token and retry";
+    public static final String INVALID_TOKEN_ERROR_MESSAGE = "Invalid Token. Please refresh token and try again";
     public static final String COGNITO_GROUPS_CLAIMS = "cognito:groups";
     public static final String CLIENT_ID_CLAIMS = "client_id";
     public static final String ROLE_PREFIX = "ROLE_";
+    public static final String USERNAME = "username";
 }
---
+++ amcart-security/src/main/java/com/nagp/amcart/security/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.nagp.amcart.security.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.Authentication;
+
+import static com.nagp.amcart.security.constant.SecurityConstant.USERNAME;
+
+@UtilityClass
+public class SecurityUtil {
+    public static String getUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt jwt) {
+            return jwt.getClaimAsString(USERNAME);
+        }
+
+        return null;
+    }
+}
