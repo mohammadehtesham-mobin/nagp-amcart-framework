da2f8c8617cd8b60636e4306cdbfe03360f64431
NAGARRO\mdmobin <md.mobin@nagarro.com>
2025-03-14 16:46:32 +0530

    allowed from multiple origin

--- amcart-security/src/main/java/com/nagp/amcart/security/config/SecurityConfig.java
+++ amcart-security/src/main/java/com/nagp/amcart/security/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://d3hbpcrmotieze.cloudfront.net"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
